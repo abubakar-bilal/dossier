42110a57f26d4007094d926d47562fae93f4fbe8
David Lamps <davidlamps@gmail.com>
2020-07-17 14:05:53 -0500

    Set Content-Type header for csv and excel files

--- lib/dossier/responder.rb
+++ lib/dossier/responder.rb
@@ -15,6 +15,7 @@ module Dossier
 
     def to_csv
       set_content_disposition!
+      controller.headers['Content-Type'] = 'text/csv'
       # TUKAIZ NOTE: This originally used report.raw_reulsts.arrays, which does not run the
       # formatters and display column logic. It was changed here, but in future it might be an issue
       controller.response_body = StreamCSV.new(*collection_and_headers(report.results.arrays))
@@ -22,6 +23,7 @@ module Dossier
 
     def to_xls
       set_content_disposition!
+      controller.headers['Content-Type'] = 'application/vnd.ms-excel'
       # TUKAIZ NOTE: This originally used report.raw_reulsts.arrays, which does not run the
       # formatters and display column logic. It is changed here, but in future it might be an issue
       given_arrays = collection_and_headers(report.results.arrays)
